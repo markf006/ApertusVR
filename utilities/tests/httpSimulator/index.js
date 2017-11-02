f90285d053c7a78ae46e9fe6199a773209ee876a
Akos Hamori <akos.hamori@sztaki.mta.hu>
2017-11-02 13:43:08 +0100

    utilities: tests: httpSimulator fixes
    Signed-off-by: Akos Hamori <akos.hamori@sztaki.mta.hu>

--- utilities/tests/httpSimulator/index.js
+++ utilities/tests/httpSimulator/index.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var LineByLineReader = require('line-by-line');
-var lr = new LineByLineReader('./apertusvr.log');
+var lr = new LineByLineReader(process.argv[2] || 'apertusvr.log');
 
 lr.on('error', function (err) {
 	console.log('line-reader: error: ', err);
@@ -9,16 +9,21 @@ lr.on('error', function (err) {
 lr.on('line', function (line) {
 	lr.pause();
 
-	console.log('line-reader: ', line);
 	var dataObj = JSON.parse(line);
-	request.post(
-		'http://localhost',
-		dataObj,
-		function (error, response, body) {
+	console.log('line-reader: ', JSON.stringify(dataObj));
+
+	var options = {
+		uri: 'http://localhost:3000/api/v1/setproperties',
+		method: 'POST',
+		json: dataObj
+	};
+
+	request.post(options, function (error, response, body) {
 			if (error) {
 				console.log('Error: ', error);
 			}
-			console.log(body);
+			console.log();
+			console.log('request: body: ', body);
 
 			lr.resume();
 		}
